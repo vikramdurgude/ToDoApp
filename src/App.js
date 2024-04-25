28892e62aa56f88f51ddacaccdd2eef53e445495
vikramdurgude <durgudevikram7744@gmail.com>
2024-04-25 18:50:58 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TodoList from './TodoList';
+
+function App() {
+  return (
+    <div className='app'>
+      <TodoList></TodoList>
+
+
+    </div>
+  )
+}
+export default App;
